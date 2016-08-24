250b5feafb85b7891237f9738ece723445e681f5
IGRINI <grigmat78@gmail.com>
2016-08-25 02:07:34 +0300

    Карта и боссы со спавнами

--- scripts/vscripts/creeps/boss_table.lua
+++ scripts/vscripts/creeps/boss_table.lua
@@ -15,11 +15,6 @@ local boss_table = {}
 function InitBossTable(bosses_table)
 	local names = 
 	{
-		["Demon_possessed"]				= "POSSESSED_SPAWN",
-		["npc_dota_custom_guardian"]	= "BOSS_CHANGE_HERO_SPAWN",
-		["npc_monk_of_love"] 			= "MONK_OF_LOVE",
-		["npc_monk_of_death"]			= "MONK_OF_DEATH",
-		["npc_boss_travaler"]			= "BOSS_TRAVALER_SPAWN",
 	}
 	local respawn_time = 300 
 	local until_respawn_time = 0
@@ -27,10 +22,6 @@ function InitBossTable(bosses_table)
 	for i, x in pairs(names) do
 		AddBossToTable(bosses_table, i, x, respawn_time, until_respawn_time)
 	end
-	
-	SetCustomBossUntilRespawnTime(bosses_table, "Demon_possessed", 300)
-	SetCustomBossRespawnTime(bosses_table, "npc_boss_travaler", 300 + math.floor(150 * RandomFloat(1.0, 2.0)))
-	--SetCustomBossRespawnTime(bosses_table, "npc_dota_custom_guardian", 10) -- DELETE IT!
 end
 
 function SetCustomBossUntilRespawnTime(boss_table, boss_name, respawn_time)
